9bbf297aaf99b076d3714e7b8219eecda8116352
Flash7227 <punk_lzi23@yahoo.com>
2024-11-08 16:40:30 +0800

    other create invoice field added

---
+++ client/app/(main)/home/_new/other.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useEffect } from "react";
+
+const formSchema = z.object({
+  product: z.string(),
+  price: z.coerce.number(),
+  item: z.string().optional(),
+  quantity: z.coerce.number(),
+  total: z.coerce.number(),
+});
+
+const Other = (props: any) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      product: props.otherRows.product,
+      price: props.otherRows.price,
+      item: props.otherRows.item,
+      quantity: props.otherRows.quantity,
+      total: props.otherRows.total,
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    values.item = props.data[values.product];
+    console.log('triggered for nothing');
+  }
+
+  return (
+    <div>
+      {props.data && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            onChange={props.updateOtherRows}
+            className="flex items-end gap-3"
+          >
+            <FormField
+              control={form.control}
+              name="product"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Бараа</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    name="product"
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Бараа сонгох" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {props.data.map((d: any, index: number) => (
+                        <SelectItem key={index} value={index.toString()}>
+                          <strong>{d.pymCdName}</strong>- {d.amount + d.vatAmt}{" "}
+                          MNT
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Үнэ</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder=""
+                      {...field}
+                      type="number"
+                      value={props.otherRows.price}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="quantity"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Ширхэг</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder=""
+                      {...field}
+                      type="number"
+                      value={props.otherRows.quantity}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="total"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Дүн</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder=""
+                      {...field}
+                      type="number"
+                      readOnly
+                      className="cursor-not-allowed"
+                      value={props.otherRows.total}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* <Button type="submit">Submit</Button> */}
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
+
+export default Other;
--- client/app/(main)/home/newInvoice.tsx
+++ client/app/(main)/home/newInvoice.tsx
@@ -1,98 +1,134 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import useFetchWrapper from "@/utils/fetchWrapper";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-} from "@/components/ui/dialog"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import PrePaid from "./_new/prepaid";
 import PostPaid from "./_new/postpaid";
+import Other from "./_new/other";
+import { Button } from "@/components/ui/button";
 
 const NewInvoice = (props: any) => {
-    const { fetchWrapper } = useFetchWrapper();
-    const [prods, setProds] = useState<any>();
-    const [prepaidRows, setPrepaidRows] = useState([]);
-    const [postpaidRows, setPostpaidRows] = useState({
-        amount: 0,
-        row: undefined,
-        charge: "endBalance"
-    });
-    const [others, setOthers] = useState();
+  const { fetchWrapper } = useFetchWrapper();
+  const [prods, setProds] = useState<any>();
+  const [prepaidRows, setPrepaidRows] = useState([]);
+  const [postpaidRows, setPostpaidRows] = useState({
+    amount: 0,
+    row: undefined,
+    charge: "endBalance",
+  });
+  const [other, setOther] = useState([]);
+  const [otherRows, setOtherRows] = useState({
+    product: "",
+    price: 0,
+    item: "",
+    quantity: 0,
+    total: 0
+  });
 
-    useEffect(() => {
-        const getProducts = async () => {
-            const url = `/api/cust/products?custId=${props.custId}`;
-            const response = await fetchWrapper(url);
-            if (response["success"]) {
-                setProds(response.data);
-                createPrepaidRows(response.data.prepaid);
-            }
-        }
-        const getOtherList = async () => {
-            const url = `/api/payment/other`;
-            const response = await fetchWrapper(url);
-            if (response["success"]) {
-                setOthers(response.data);
-            }
-        }
-        getProducts();
-        getOtherList();
-    }, []);
-    const createPrepaidRows = (data:any) => {
-        const temp = data.map((obj: any) => ({
-          ...obj,
-          bulkremains: "0",
-          bulkmonth: "0",
-        }));
-        setPrepaidRows(temp);
-      };
-    const updatePostpaidRows = (amount:any, row:any, charge:any) => {
-        if(charge =="custom" && amount){
-            amount = amount.replace(",", "");
-            amount = parseFloat(amount);
-        }
-        if(charge != "custom" && row){
-            amount = row[charge];
-        }
-        setPostpaidRows({
-            amount: amount,
-            row: row,
-            charge: charge
-        });
-        console.log(amount, row, charge);
+  useEffect(() => {
+    const getProducts = async () => {
+      const url = `/api/cust/products?custId=${props.custId}`;
+      const response = await fetchWrapper(url);
+      if (response["success"]) {
+        setProds(response.data);
+        createPrepaidRows(response.data.prepaid);
+      }
+    };
+    const getOtherList = async () => {
+      const url = `/api/payment/other`;
+      const response = await fetchWrapper(url);
+      if (response["success"]) {
+        setOther(response.data);
+      }
+    };
+    getProducts();
+    getOtherList();
+  }, []);
+  const createPrepaidRows = (data: any) => {
+    const temp = data.map((obj: any) => ({
+      ...obj,
+      bulkremains: "0",
+      bulkmonth: "0",
+    }));
+    setPrepaidRows(temp);
+  };
+  const updatePostpaidRows = (amount: any, row: any, charge: any) => {
+    if (charge == "custom" && amount) {
+      if (typeof amount != "number") {
+        amount = amount.replaceAll(",", "");
+        amount = parseFloat(amount);
+      }
     }
-    return (
-        <Dialog open={props.custId} onOpenChange={props.handleNewInvoiceClose}>
-            <DialogContent className="max-w-[90%] max-h-[90%] overflow-y-auto">
-                <DialogHeader>
-                    <DialogTitle>{props.custId} - Шинэ нэхэмжлэл</DialogTitle>
-                    <DialogDescription>
-                    </DialogDescription>
-                </DialogHeader>
-                <div>
-                    <Tabs defaultValue="prepaid">
-                        <TabsList>
-                            <TabsTrigger value="prepaid">Урьдчилсан төлбөрт</TabsTrigger>
-                            <TabsTrigger value="postpaid">Дараа төлбөрт</TabsTrigger>
-                            <TabsTrigger value="other">Бусад төлбөр</TabsTrigger>
-                        </TabsList>
-                        <TabsContent value="prepaid">
-                            {
-                                prods &&  <PrePaid data={prepaidRows} setPrepaidRows={setPrepaidRows}/>
-                            }
-                        </TabsContent>
-                        <TabsContent value="postpaid">{
-                             prods &&   <PostPaid data={prods.postpaid} postpaidRows={postpaidRows} updatePostpaidRows={updatePostpaidRows}/>}</TabsContent>
-                        <TabsContent value="other">Change your password here.</TabsContent>
-                    </Tabs>
-                </div>
-            </DialogContent>
-        </Dialog>
-    );
-}
+    if (charge != "custom" && row) {
+      amount = row[charge];
+    }
+    setPostpaidRows({
+      amount: amount,
+      row: row,
+      charge: charge,
+    });
+  };
+  const updateOtherRows = (values:any)=>{
+    const name = values.target.name;
+    const value = values.target.value;
+    if(name == 'product'){
+      console.log(other[value]["amount"] + other[value]["vatAmt"]);
+      setOtherRows({...otherRows, product: value, item: other[value], quantity: 0, total: other[value]["amount"] + other[value]["vatAmt"], price: other[value]["amount"] + other[value]["vatAmt"]});
+    }else if(name == "price"){
+      setOtherRows({...otherRows, total: value * otherRows.quantity, price: value});
+    }else if(name == "quantity"){
+      setOtherRows({...otherRows, quantity: value, total: value * otherRows.price});
+    }
+  }
+  return (
+    <Dialog open={props.custId} onOpenChange={props.handleNewInvoiceClose}>
+      <DialogContent className="max-w-[90%] max-h-[90%] overflow-y-auto">
+        <DialogHeader>
+          <DialogTitle>{props.custId} - Шинэ нэхэмжлэл</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <div>
+          <Tabs defaultValue="prepaid">
+            <div className="flex justify-between">
+              <TabsList>
+                <TabsTrigger value="prepaid">Урьдчилсан төлбөрт</TabsTrigger>
+                <TabsTrigger value="postpaid">Дараа төлбөрт</TabsTrigger>
+                <TabsTrigger value="other">Бусад төлбөр</TabsTrigger>
+              </TabsList>
+              <div>
+                <Button>Тооцох</Button>
+              </div>
+            </div>
+
+            <TabsContent value="prepaid">
+              {prods && (
+                <PrePaid data={prepaidRows} setPrepaidRows={setPrepaidRows} />
+              )}
+            </TabsContent>
+            <TabsContent value="postpaid">
+              {prods && (
+                <PostPaid
+                  data={prods.postpaid}
+                  postpaidRows={postpaidRows}
+                  updatePostpaidRows={updatePostpaidRows}
+                />
+              )}
+            </TabsContent>
+            <TabsContent value="other">
+              <Other data={other} otherRows={otherRows} updateOtherRows={updateOtherRows}/>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default NewInvoice;
